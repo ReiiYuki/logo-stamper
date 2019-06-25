77879821b308d3455774bc7aca9a65f5dfc72f92
voraton.l <voraton@wongnai.com>
2019-06-26 01:58:14 +0700

    up and down

--- src/App.js
+++ src/App.js
@@ -1,26 +1,72 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import { Text, PaddingBox, Card, CardAreaMain, CardMedia, Button } from 'reactackle'
+import ImageUploader from 'react-images-upload' 
+import styled from 'styled-components'
+import Zip from 'jszip'
+import { saveAs } from 'file-saver'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const ImgCard = ({ src }) => (
+  <div style={{ width: '100%', maxWidth: '400px' }}>
+    <Card>
+      <CardAreaMain>
+        <CardMedia mediaElement={<img style={{ width: '100%', maxWidth: '400px' }} src={src} alt="" />} />
+      </CardAreaMain>
+    </Card>
+  </div>
+)
+
+const CTN = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  width: 100%;
+  > div {
+    margin: 16px;
+  }
+`
+
+class App extends Component {
+  state = {
+    inputPic: []
+  }
+  onDrop = (pictureFiles, pictureDataURLs) => {
+    this.setState({
+      inputPic: pictureDataURLs,
+    })
+  }
+  download = () => {
+    // change this to stamped image soon
+    const zip = new Zip()
+    this.state.inputPic.map((data, index) => zip.file(`${index}.jpg`, data.split(',')[1], { base64: true }))
+    zip.generateAsync({type:"blob"}).then(content => {
+      saveAs(content)
+    })
+  }
+  render() {
+    return (
+      <>
+        <PaddingBox paddingSize="large">
+          <Text display="headline">Input</Text>
+          <ImageUploader
+            withIcon={true}
+            buttonText='Choose images'
+            onChange={this.onDrop}
+            imgExtension={['.jpg', '.jpeg', '.png']}
+            maxFileSize={5242880}
+          />
+          <CTN>
+            {this.state.inputPic.map(data => <ImgCard key={data} src={data} />)}
+          </CTN>
+        </PaddingBox>
+        <PaddingBox paddingSize="large">
+          <Text display="headline">Output</Text>
+          <CTN>
+
+          </CTN>
+          <Button colorScheme="primary" text="Download" size="large" onPress={this.download} />
+        </PaddingBox>
+      </>
+    )
+  }
 }
 
-export default App;
+export default App
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import {
+  injectGlobalStyle,
+  injectResetStyle,
+} from 'reactackle';
+
+injectGlobalStyle();
+injectResetStyle();
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
